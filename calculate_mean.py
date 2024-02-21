d17d2f5e528bda0322f63559cb61577ae5340b8e
LabGoBoom <gagic.mladen@gmail.com>
2024-02-21 15:09:24 +0100

    add dosctring

--- calculate_mean.py
+++ calculate_mean.py
@@ -1,5 +1,8 @@
 import numpy as np
 
 def calculate_mean(data):
+"""This function computes the mean of an array.
+Input: An one dimentional array
+Output: the mean of the array"""
 	mean=np.mean(data)
 	return mean
